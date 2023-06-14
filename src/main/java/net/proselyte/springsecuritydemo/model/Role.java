a461bfc0cd2da7c4b7f6c8d688cba58ac989f155
OlegZ <beyondswamps@gmail.com>
2023-06-14 11:26:20 +0300

    5 step completed. Added permissions

--- src/main/java/net/proselyte/springsecuritydemo/model/Role.java
+++ src/main/java/net/proselyte/springsecuritydemo/model/Role.java
@@ -1,6 +1,28 @@
 package net.proselyte.springsecuritydemo.model;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public enum Role {
-    USER,
-    ADMIN;
+    ADMIN(Set.of(Permission.DEVELOPERS_WRITE, Permission.DEVELOPERS_READ)),
+    USER(Set.of(Permission.DEVELOPERS_READ));
+
+    private Set<Permission> permissions;
+
+    Role(Set<Permission> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public Set<SimpleGrantedAuthority> getAuthorities() {
+        return getPermissions().stream()
+                .map(permission -> new SimpleGrantedAuthority(permission.getPermission()))
+                .collect(Collectors.toSet());
+    }
 }
